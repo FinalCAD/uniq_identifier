d937f97b74b42064bef5c9a9a03279125ff4083a
Joel AZEMAR <joel.azemar@gmail.com>
2014-10-08 16:30:14 +0200

    break point

---
+++ lib/generators/uniq_identifier/add/templates/migration.rb
@@ -0,0 +1,8 @@
+class UniqIdentifierAddUuid<%= class_name %> < ActiveRecord::Migration
+  def change
+    change_table(:<%= plural_name %>) do |t|
+      t.string :uuid
+    end
+    add_index(:<%= plural_name %>, :uuid)
+  end
+end
--- lib/generators/uniq_identifier/install/install_generator.rb
+++ lib/generators/uniq_identifier/install/install_generator.rb
@@ -1,24 +1,39 @@
-require 'rails/generators/active_record'
+# require 'pry'
+# require 'rails/generators/active_record'
 
 module UniqIdentifier
   module Generators
-    class InstallGenerator < Rails::Generators::NamedBase
+    class InstallGenerator < Rails::Generators::Base
       include Rails::Generators::Migration
 
       source_root File.expand_path('../templates', __FILE__)
 
-      def self.next_migration_number(path)
-        ActiveRecord::Generators::Base.next_migration_number(path)
-      end
+      # def self.next_migration_number(path)
+      #   ActiveRecord::Generators::Base.next_migration_number(path)
+      # end
+
+      # :migration_file_name, :migration_class_name
 
       desc <<DESC
 description :
     copy uniq_identifier configuration to an initializer.
+    rails generate uniq_identifier:install
 DESC
       def copy_initializer_file
+        # binding.pry
         template 'uniq_identifier.rb', "config/initializers/uniq_identifier.rb"
-        migration_template 'migration.rb', "db/migrate/add_uuid_user.rb"
+      #   migration_template 'migration.rb', "db/migrate/add_uuid_user.rb"
+      #   # migration_template "migration.rb", "db/migrate/add_foo_to_bar.rb"
+      #         hook   = 'uniq_identifier'
+      # data   = "\n"
+      # data += indent("#{hook}")
+      # header = "class #{class_name} < ActiveRecord::Base"
+      # model_path = File.join('app', 'models', "#{file_path}.rb")
+      # binding.pry
+      # inject_into_file model_path, data, after: /header/, verbose: true
+      #
       end
     end
+
   end
 end
