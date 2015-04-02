d68dafb528a3e914e0bfc36aa5086455f5138e26
Joel AZEMAR <joel.azemar@gmail.com>
2015-04-02 10:59:37 +0200

    for avoid manual attempts to set uuid at nil

--- lib/uniq_identifier.rb
+++ lib/uniq_identifier.rb
@@ -9,5 +9,6 @@ module UniqIdentifier
   def uniq_identifier
     include Hook
     after_initialize :set_uniq_identifier
+    after_validation :set_uniq_identifier # Case of manual attempt to do a nil assignation 
   end
 end
