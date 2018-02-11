26c8c652dc5a42a03592f4939fe23c5a282657d6
Thomas Riboulet <riboulet@gmail.com>
2018-02-11 22:09:41 +0100

    RD-1545 use gemfury
    JIRA : https://finalcad.atlassian.net/browse/RD-1545
    
    add script to build gem and push it to finalcad gemfury account
    
    requires FURY_TOKEN in CI env

---
+++ bin/package_push.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#
+# ENV VAR to setup in CI : FURY_TOKEN
+
+for file in `ls *gemspec` do
+    gem build $file
+done
+for file in `ls *gem`; do
+    curl -F package=@$file "https://${FURY_TOKEN}@push.fury.io/finalcad/"
+done
